be820c9a166834e90e326cf737a34048b41e4b99
Marc Tyndel <marctyndel@icloud.com>
2019-07-09 16:50:02 -0400

    remove try block for increasing simplicity

--- experiments/text_generation_simple/main.py
+++ experiments/text_generation_simple/main.py
@@ -1,59 +1,48 @@
-"""
-This file is used for your model code, and for organizing and executing Foundations stages.
-
-Below we import foundations, as well as a function from our model file that we'll use as a stage.
-
-We then tell Foundations how to run by defining a path to a configuration file.
-
-Next we define a project name using `foundations.set_project_name()`.
-
-We define the experiment to run as `result` and then use Foundations's `.run()` method which tells Foundations to run these stages.
-
-Then you can run the driver file with `python main.py` to send the experiment off to be run. To check results, see the `/results` directory where you'll read and interact with results.
-"""
-from utils import post_slack_channel
 import time
+from preprocessing import download_data, preprocess_data
+from model import Model
+from utils import save_preprocessors, load_preprocessors
 
-try:
-    from preprocessing import download_data, preprocess_data
-    from model import Model
-    from utils import get_params, save_preprocessors, load_preprocessors
 
-    path_to_file = download_data('https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt', 'shakespeare.txt')
+params = {
+    "rnn_units": 1024,
+    "batch_size": 64,
+    "embedding_dim": 256,
+    "epochs": 3,
+    "seq_length": 100,
+    "temperature": 1.,
+    "dataset_url": "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
+}
 
-    dataset_train, dataset_test, steps_per_epoch_train, steps_per_epoch_test, vocab, char2idx, idx2char = preprocess_data(path_to_file, params)
+path_to_file = download_data('https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt', 'shakespeare.txt')
 
-    # Save preprocessors for serving
-    save_preprocessors(char2idx, idx2char, vocab)
+dataset_train, dataset_test, steps_per_epoch_train, steps_per_epoch_test, vocab, char2idx, idx2char = preprocess_data(path_to_file, params)
 
-    model = Model(vocab,
-                  embedding_dim=params['embedding_dim'],
-                  rnn_units=params['rnn_units'],
-                  batch_size=params['batch_size'],
-                  char2idx=char2idx,
-                  idx2char=idx2char)
+# Save preprocessors for serving
+save_preprocessors(char2idx, idx2char, vocab)
 
-    model.train(dataset_train,
-                steps_per_epoch=steps_per_epoch_train,
-                checkpoint_dir='./training_checkpoints',
-                epochs=params['epochs'],)
+model = Model(vocab,
+              embedding_dim=params['embedding_dim'],
+              rnn_units=params['rnn_units'],
+              batch_size=params['batch_size'],
+              char2idx=char2idx,
+              idx2char=idx2char)
 
-    train_loss = model.test(dataset_train, steps_per_epoch_train)
-    print(train_loss)
+model.train(dataset_train,
+            steps_per_epoch=steps_per_epoch_train,
+            checkpoint_dir='./training_checkpoints',
+            epochs=params['epochs'],)
 
-    test_loss = model.test(dataset_test, steps_per_epoch_test)
-    print(test_loss)
+train_loss = model.test(dataset_train, steps_per_epoch_train)
+print(train_loss)
 
-    model.set_test_mode(checkpoint_dir='./training_checkpoints')
+test_loss = model.test(dataset_test, steps_per_epoch_test)
+print(test_loss)
 
-    start_time = time.time()
-    generated_text = model.generate(start_string=u"ROMEO: ", temperature=params['temperature'])
-    print('synthesis time: {}'.format(time.time() - start_time))
-    print(generated_text)
-    
-except Exception as e:
-    import traceback
-    
-    trace = traceback.format_exc()
-    post_slack_channel('Job failed. Catching Exception:\n{}'.format(trace))
-    raise e
+model.set_test_mode(checkpoint_dir='./training_checkpoints')
+
+start_time = time.time()
+generated_text = model.generate(start_string=u"ROMEO: ", temperature=params['temperature'])
+print('synthesis time: {}'.format(time.time() - start_time))
+print(generated_text)
+
