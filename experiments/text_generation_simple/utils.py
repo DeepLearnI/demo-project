db9eb8c3df9ec9cae7d92993386018d461d89486
Marc Tyndel <marctyndel@icloud.com>
2019-07-09 16:55:46 -0400

    moved params out of utils for simplicity

--- experiments/text_generation_simple/utils.py
+++ experiments/text_generation_simple/utils.py
@@ -5,18 +5,15 @@ import pickle
 slack_client = SlackClient('xoxp-6264324945-46768961556-314329155110-1f92449856f0d07a99f7598ff31e9bf2')
 
 
-def get_params():
-    params = {
-        "rnn_units": 1024,
-        "batch_size": 64,
-        "embedding_dim": 256,
-        "epochs": 3,
-        "seq_length": 100,
-        "temperature": 1.,
-        "dataset_url": "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
-    }
-
-    return params
+params = {
+    "rnn_units": 1024,
+    "batch_size": 64,
+    "embedding_dim": 256,
+    "epochs": 3,
+    "seq_length": 100,
+    "temperature": 1.,
+    "dataset_url": "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
+}
 
 
 def post_slack_channel(msg):
